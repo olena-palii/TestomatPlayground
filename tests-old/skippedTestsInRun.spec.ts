31f61a4fb1e99a7335de23543f4e8d567ba170d2
Olena Palii <8882845+olena-palii@users.noreply.github.com>
2026-02-28 23:08:59 +0100

    Update sync script

--- tests-old/skippedTestsInRun.spec.ts
+++ tests-old/skippedTestsInRun.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe.skip("Skipped Tests in Run @Sf57254d0", () => {
+test.describe("Skipped Tests in Run @Sf57254d0", () => {
     test('Test 1 (10 seconds) @Tc46ac865', async () => {
         console.log('Test 1 (10 seconds) started: ' + new Date().toLocaleString());
         await new Promise(resolve => setTimeout(resolve, 10000));
