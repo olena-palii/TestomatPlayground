18be49a585a75e99428dbecb33d2945a5a138835
Olena Palii <8882845+olena-palii@users.noreply.github.com>
2026-03-01 00:28:06 +0100

    Add fixtures

--- deTest.ts
+++ deTest.ts
@@ -1,5 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-export const deTest = test.extend({});
+type DeFixtures = {
+    deLog: {
+        logStart: () => void;
+        logFinish: () => void;
+    };
+    deWaiter: {
+        waitSeconds: (n: number) => Promise<void>;
+        waitMinutes: (n: number) => Promise<void>;
+    };
+};
+
+export const deTest = test.extend<DeFixtures>({
+    deWaiter: async ({}, use) => {
+        await use({
+            waitSeconds: (n: number) => new Promise(resolve => setTimeout(resolve, n * 1000)),
+            waitMinutes: (n: number) => new Promise(resolve => setTimeout(resolve, n * 60 * 1000)),
+        });
+    },
+    deLog: async ({ }, use, testInfo) => {
+        await use({
+            logStart: () => console.log(`[${new Date().toISOString()}] "${testInfo.title}"started`),
+            logFinish: () => console.log(`[${new Date().toISOString()}] "${testInfo.title}" finished`),
+        });
+    },
+});
 
 export const deExpect = expect.extend({});
--- tests/parameterizedTestsInLoop.spec.ts
+++ tests/parameterizedTestsInLoop.spec.ts
@@ -6,20 +6,30 @@ deTest.describe("Parameterized Tests in Loop @S49731617", () => {
     const testValues1: Array<FruitsType> = ["apple", "banana", "orange", "grape", "kiwi"];
     for (const testValue of testValues1) {
         const fruitName = testValue.toUpperCase();
-        deTest(`API 1.0 - TestA - ${fruitName} @Tf14fa20d`, async () => {
+        deTest(`API 1.0 - TestA - ${fruitName} @Tf14fa20d`, async ({ deLog, deWaiter }) => {
+            deLog.logStart();
+            await deWaiter.waitSeconds(1);
             deExpect(1 + 1).toBe(2);
+            deLog.logFinish();
         });
-        deTest(`API 1.0 - TestB - ${fruitName} @T14e9c507`, async () => {
+        deTest(`API 1.0 - TestB - ${fruitName} @T14e9c507`, async ({ deLog, deWaiter }) => {
+            deLog.logStart();
+            await deWaiter.waitSeconds(1);
             deExpect(1 + 1).toBe(2);
+            deLog.logFinish();
         });
     }
     const testValues2 = ["CAR", "BIKE", "BUS"];
     for (const testValue of testValues2) {
-        deTest(`API 2.0 - TestA - ${testValue} @Tfc05ed0d`, async () => {
+        deTest(`API 2.0 - TestA - ${testValue} @Tfc05ed0d`, async ({ deLog }) => {
+            deLog.logStart();
             deExpect(1 + 1).toBe(2);
+            deLog.logFinish();
         });
-        deTest(`API 2.0 - TestB - ${testValue} @T54cb7790`, async () => {
+        deTest(`API 2.0 - TestB - ${testValue} @T54cb7790`, async ({ deLog }) => {
+            deLog.logStart();
             deExpect(1 + 1).toBe(2);
+            deLog.logFinish();
         });
     }
 });
