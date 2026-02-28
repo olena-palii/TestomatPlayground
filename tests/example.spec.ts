1c94923a216fac4afe311de481656b2087c88080
Olena Palii <8882845+olena-palii@users.noreply.github.com>
2026-02-28 22:56:22 +0100

    Update example test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('has title @T4e4c31de', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link @T544f552f', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('1 + 1 = 2 @T4e4c31de', async ({ page }) => {
+  await expect(1 + 1).toBe(2);
 });
