c59cade525b240decddb091270498ba3db604eeb
Olena Palii <8882845+olena-palii@users.noreply.github.com>
2026-03-01 00:09:25 +0100

    Add deTest and deExpect as custom test and expect

---
+++ deTest.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test';
+
+export const deTest = test.extend({});
+
+export const deExpect = expect.extend({});
--- tests/parameterizedTestsInLoop.spec.ts
+++ tests/parameterizedTestsInLoop.spec.ts
@@ -1,25 +1,25 @@
-import { test, expect } from '@playwright/test'
+import { deTest, deExpect } from '../deTest'
 
 type FruitsType = "apple" | "banana" | "orange" | "grape" | "kiwi";
 
-test.describe("Parameterized Tests in Loop @S49731617", () => {
+deTest.describe("Parameterized Tests in Loop @S49731617", () => {
     const testValues1: Array<FruitsType> = ["apple", "banana", "orange", "grape", "kiwi"];
     for (const testValue of testValues1) {
         const fruitName = testValue.toUpperCase();
-        test(`API 1.0 - TestA - ${fruitName} @Tf14fa20d`, async () => {
-            expect(1 + 1).toBe(2);
+        deTest(`API 1.0 - TestA - ${fruitName} @Tf14fa20d`, async () => {
+            deExpect(1 + 1).toBe(2);
         });
-        test(`API 1.0 - TestB - ${fruitName} @T14e9c507`, async () => {
-            expect(1 + 1).toBe(2);
+        deTest(`API 1.0 - TestB - ${fruitName} @T14e9c507`, async () => {
+            deExpect(1 + 1).toBe(2);
         });
     }
     const testValues2 = ["CAR", "BIKE", "BUS"];
     for (const testValue of testValues2) {
-        test(`API 2.0 - TestA - ${testValue} @Tfc05ed0d`, async () => {
-            expect(1 + 1).toBe(2);
+        deTest(`API 2.0 - TestA - ${testValue} @Tfc05ed0d`, async () => {
+            deExpect(1 + 1).toBe(2);
         });
-        test(`API 2.0 - TestB - ${testValue} @T54cb7790`, async () => {
-            expect(1 + 1).toBe(2);
+        deTest(`API 2.0 - TestB - ${testValue} @T54cb7790`, async () => {
+            deExpect(1 + 1).toBe(2);
         });
     }
 });
