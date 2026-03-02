d0368ddd7bd3260c8cb74a853448bfaa4f5462ff
Olena Palii <8882845+olena-palii@users.noreply.github.com>
2026-03-02 20:35:57 +0100

    Add Skipped Tests in Short Run

---
+++ tests/skippedTestsInShortRun.spec.ts
@@ -0,0 +1,64 @@
+import { test, expect } from '@playwright/test'
+
+test.describe("Skipped Tests in Short Run @S583f2ad7", () => {
+    test('Test 1 @Tc46ac865', async () => {
+        console.log('Test 1 started: ' + new Date().toLocaleString());
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        expect(1 + 1).toBe(2)
+        console.log('Test 1 finished: ' + new Date().toLocaleString());
+    });
+    test('Test 2 @T329952ec', async () => {
+        console.log('Test 2 started: ' + new Date().toLocaleString());
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        expect(1 + 1).toBe(2)
+        console.log('Test 2 finished: ' + new Date().toLocaleString());
+    });
+    test('Test 3 @Tdfe8e40d', async () => {
+        console.log('Test 3 started: ' + new Date().toLocaleString());
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        expect(1 + 1).toBe(2)
+        console.log('Test 3 finished: ' + new Date().toLocaleString());
+    });
+    test('Test 4 @Te50b58de', async () => {
+        console.log('Test 4 started: ' + new Date().toLocaleString());
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        expect(1 + 1).toBe(2)
+        console.log('Test 4 finished: ' + new Date().toLocaleString());
+    });
+    test.skip('Test 5 @Ta3277677', async () => {
+        console.log('Test 5 started: ' + new Date().toLocaleString());
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        expect(1 + 1).toBe(2)
+        console.log('Test 5 finished: ' + new Date().toLocaleString());
+    });
+    test.skip('Test 6 @T02851fb5', async () => {
+        console.log('Test 6 started: ' + new Date().toLocaleString());
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        expect(1 + 1).toBe(2)
+        console.log('Test 6 finished: ' + new Date().toLocaleString());
+    });
+    test('Test 7 @T383d6c0c', async () => {
+        console.log('Test 7 started: ' + new Date().toLocaleString());
+        await new Promise(resolve => setTimeout(resolve, 60000));
+        expect(1 + 1).toBe(2)
+        console.log('Test 7 finished: ' + new Date().toLocaleString());
+    });
+    test('Test 8 @Tc752e661', async () => {
+        console.log('Test 8 started: ' + new Date().toLocaleString());
+        await new Promise(resolve => setTimeout(resolve, 60000));
+        expect(1 + 1).toBe(2)
+        console.log('Test 8 finished: ' + new Date().toLocaleString());
+    });
+    test.skip('Test 9 @T4523d85c', async () => {
+        console.log('Test 9 started: ' + new Date().toLocaleString());
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        expect(1 + 1).toBe(2)
+        console.log('Test 9 finished: ' + new Date().toLocaleString());
+    });
+    test.skip('Test 10 @T0d4b7956', async () => {
+        console.log('Test 10 started: ' + new Date().toLocaleString());
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        expect(1 + 1).toBe(2)
+        console.log('Test 10 finished: ' + new Date().toLocaleString());
+    });
+});
